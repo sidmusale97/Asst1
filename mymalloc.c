31ae9f32af2225212e42efd5956059a826952533
sidmusale97 <ssm168@rutgers.edu>
2018-02-06 19:12:47 -0500

    Update mymalloc.c

--- mymalloc.c
+++ mymalloc.c
@@ -1,30 +1,8 @@
 #include "mymalloc.h"
-#include <ctype.h>
-static char myblock[5000];
-void * mymalloc(int size);
-int findIndex(int size);
 
-int main()
-{
-	mymalloc(5);
-}
+
 char * mymalloc(int size)
 {
-	if (size == 0)
-		return 
-	for(i = 0; i <5000; i++)
-	{
-		if (isdigit(myblock[i]))//if header byte found
-		{
-			i = i + myblock[i];	//jump past reserved bytes
-			if(i > 5000)return -1;//if jump is out of bounds return error
-			continue;
-		}
-		else
-		{
-			if((i + size) > 5000)return -1;//if index found but requested memory is out of bounds
-			myblock[i] = size;
-		}
-	}
-	return *myblock[i]; 
+	if (size == 0)return NULL;
+	 
 }
