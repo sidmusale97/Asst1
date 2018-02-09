559684cb08ad23fc8eb118a522215a2377d38305
Rsakhuja <36058972+Rsakhuja@users.noreply.github.com>
2018-02-09 11:42:33 -0500

    Add files via upload

--- mymalloc.h
+++ mymalloc.h
@@ -8,17 +8,20 @@
 
 
 #include <stdio.h>
+#include<stddef.h>
 
 
 static char myblock[5000];  //array size simulated
 
 typedef struct metaData{
     int size; //size of data
-    int isFree;       //flag to indicate if block is empty or not
+    int isFree;       //flag to indicate if block is empty or not: 0 = empty, 1 = not empty
     struct metaData *next;  //pointer to next block
 }metaData;
 
-metaData *memoryArray = (char*) myblock; //points to first block of memory
+metaData *blockPtr = (char*) myblock;  //sets pointer to first index in array
+
+
 
 
 
