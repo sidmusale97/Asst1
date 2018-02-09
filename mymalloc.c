d9f5beb30a0023a1a3a9e7209e7522b793ca7917
Rsakhuja <36058972+Rsakhuja@users.noreply.github.com>
2018-02-09 11:37:06 -0500

    Add files via upload

--- mymalloc.c
+++ mymalloc.c
@@ -11,25 +11,26 @@
 #include "mymalloc.h"
 
 void initialize(){
-    memoryArray->size = 5000 - sizeof(metaData);
-    memoryArray->isFree = 1;
-    memoryArray->next = NULL;
+    blockPtr->size = 5000 - sizeof(metaData);
+    blockPtr->isFree = 0;
+    blockPtr->next = NULL;
 }
 
-
 //if first block found is greater than required size, then split
-char* split(metaData *allocatedBlock, int requiredSize){
+char* allocate(metaData *allocatedBlock, int requiredSize){
     metaData *requiredBlock = (char*)(char*) requiredSize + sizeof(metaData) ;
     requiredBlock->size = requiredSize;
     allocatedBlock->size = allocatedBlock->size - requiredSize - sizeof(metaData);
-    requiredBlock->isFree = 0; //slot is not longer free
-    allocatedBlock->isFree = 1; //allocated slot is still free
+    requiredBlock->isFree = 0; //slot is still free
+    allocatedBlock->isFree = 1; //allocated slot is no longer free
     requiredBlock->next = allocatedBlock;
     allocatedBlock->next = NULL;
     return requiredBlock;
 }
-char* MyMalloc(int size){
-    metaData *current, *previous;
+
+void* my_malloc(int size){
+    metaData *current = myblock;
+    metaData*previous;
     while(((current->size < size) || (current->isFree == 0)) && (current->next != NULL)){
         previous = current;
         current = current ->next;
@@ -37,5 +38,26 @@ char* MyMalloc(int size){
     if(current->size == size){
         return previous;
     }else{
-        split(current, size);
+      return allocate(current, size);
     }
+    printf("%s", "Space has been allocated");
+}
+
+void merge(){
+    metaData *current,*previous = myblock;
+    while(current->next != NULL){
+        while((current->isFree == 1) && (previous->isFree ==1)){
+            previous = current;
+            current = current->next;
+        }
+        previous->size = previous->size + current->size + sizeof(metaData);
+        previous->isFree = 0;
+        current = current->next;
+        previous->next = current;
+    }
+    printf("%s", "MERGED");
+   
+}
+void my_free(){
+    
+}
