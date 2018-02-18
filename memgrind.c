55205ddc80c69fedb19be9fda493438f4280bc5e
Rsakhuja <36058972+Rsakhuja@users.noreply.github.com>
2018-02-17 23:06:08 -0500

    Add files via upload

--- memgrind.c
+++ memgrind.c
@@ -1,131 +1,282 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "mymalloc.c"
-int main()
+#include "mymalloc.h"
+#include <time.h>
+
+int main1()
 {
-    void * p = malloc(5000);
+    clock_t start,end;
+    double elapsedA=0,elapsedB=0,elapsedC=0, elapsedD=0, elapsedE=0, elapsedF = 0;
+    double totalRunTimeA=0, totalRunTimeB=0,totalRunTimeC=0,totalRunTimeD=0,totalRunTimeE=0, totalRunTimeF = 0;
+
+   int programCounter = 0;
+//// while(programCounter <100){
+
+//Workload A: Malloc() 1 byte and immediately free it x150
+//    start = clock();
+//    int i;
+//    for (i = 0; i < 150;i++)
+//    {
+//        printblocks("main-1................\n");
+//
+//        void * p = my_malloc(1);
+//
+//        //printf("main-1...validity=%i %p\n", isPointerValid(p),p);
+//
+//        my_free(p);
+//
+//        printblocks("main-2.................\n");
+//    }
+//    end = clock();
+//
+//        //Manage RunTimeA for iteration
+//        elapsedA =(double)( end - start);
+//        totalRunTimeA = totalRunTimeA + elapsedA;
+//        end = 0; start = 0; //reset end and start markers for next workload
+//
+//
+//    //Workload B: Malloc(), store pointer in arrayx150 times
+//        start = clock();
+//        char * ptrs[150];
+//        int i = 0;
+//        for(i = 0; i < 150;i++)
+//        {
+//            ptrs[i] = my_malloc(1);
+//           printblocks("main-B");
+//        }
+//        freeall();
+//    printblocks("main-B2");
+//        end = clock();
+//
+//
+//        //Manage RunTimeB for iteration
+//        elapsedB = (double)(end - start);
+//        totalRunTimeB = totalRunTimeB + elapsedB;
+//        end = 0; start = 0; //reset end and start maker for next workload
+
+        //Workload C: Randomly choose b/w 1 byte malloc and 1 byte free, continue until 150 mallocs
+//
+//    start = clock();
+    int mallocCounter = 0;  //counts total number of mallocs
+    int freeCounter = 0;
+    char*p = NULL;
+    while(mallocCounter < 150){
+        if(mallocCounter == 0){ //if no byte has been allocated, then first malloc
+            p = my_malloc(1);
+            mallocCounter++;
+        }
+
+        int operation = rand() % 2;
+        printf("operation: %d\n", operation);
+
+        if(operation == 1){           //if r = 1 malloc 1 byte
+            p = my_malloc(1);
+             printblocks("Allocate");
+            mallocCounter++;
+            if(mallocCounter >= 150)break;
+        }else{
+            // logic for deciding which block to free
+            // free the first block from the begiing
+            
+            metaData *a= getBlockPointer(); //blockPtr;
+            
+            printf("a=%p  %p",a,a->next);
+            
+//            while(a!=NULL){
+//                if(!(a->isFree)){
+//                    my_free(++a);
+//                    break;
+//                }
+//                a = a->next;
+//            }
+//
+            if(a->next != NULL){
+                // that means there is a allocated bloc - just release it as its the first block
+                my_free(a->next);
+                freeCounter++;
+                printblocks("Free");
+
+            } else {
+                printf("why null  %d \n",(mallocCounter-freeCounter));
+            }
+//        }
+//    }
+    freeall();
+   
+//    end = clock();
+
+    //Manage RunTimeC for iteration
+//    elapsedC = (double)(end - start);
+//    totalRunTimeC = totalRunTimeC + elapsedC;
+//    end = 0; start = 0; //reset end and start for next workload
+    
+//        //Workload D: Randomly-sized malloc() or free() pointers
+//    start = clock();
+//    int mallocSize = 0; //size of malloc bytes
+//    int operation = 0;  //determines free or malloc
+//    int mallocCounter = 0;     //keeps track of total number of mallocs
+//    int pointersMalloc = 0; //keeps track of number of current pointers malloc
+//    char *ptr = NULL;
+//    while(mallocCounter <= 5000){
+//        if(mallocCounter ==0 || pointersMalloc ==0){
+//            mallocSize = (int) rand%64;
+//            ptr= my_malloc(mallocSize);
+//            mallocCounter++;
+//            pointersMalloc++;
+//        }
+//        operation = rand()%2;
+//        if(operation ==1){
+//            mallocSize = rand()%64;
+//            ptr= my_malloc(mallocSize);
+//            mallocCounter++;
+//            if(mallocCounter >150)break;
+//            pointersMalloc++;
+//        }else{
+//            if(ptr==NULL){
+//                ptr--;
+//            }else{
+//               my_free(ptr);
+////            metaData *a= blockPtr;
+////            while(a!=NULL){
+////                if(a->isFree){
+////                    --a;
+////                }
+////                if(!(a->isFree)){
+////                    my_free(++a);
+////                    break;
+////                }
+////                //a = a->next;
+////            }
+//        }
+//    }
+//    freeall();
+//    end = clock();
+//
+//        //Manage RunTimeD for iteration
+//        elapsedD = (double)(end - start);
+//        totalRunTimeD = totalRunTimeD + elapsedD;
+//        end = 0; start = 0;
+
+//        //Workload E: Malloc randomly sized chunk random number of times (size of chunk stays consistent) and free
+//        start = clock();
+//        char *ptrE = NULL;
+//        mallocSize = rand()%5000;    //size of malloc chunks
+//        mallocCounter = rand()%25;    //total number of malloc sizes (must be b/w 1 and 150)
+//        int remainingSize = 5000;     //remaining size of memory after each chunk is malloc
+//        int counter = 0;
+//        while(counter != mallocCounter || (remainingSize < mallocSize)){
+//            ptrE = my_malloc(mallocSize);
+//            remainingSize = remainingSize - mallocSize;
+//            counter++;
+//        }
+//        while(mallocCounter >0){
+//            my_free(ptrE);
+//            mallocCounter --;
+//        }
+//        end = clock();
+//
+//        //Manage RunTimeE for iteration
+//        elapsedE = (double)(end - start);
+//        totalRunTimeE = totalRunTimeE+ elapsedE;
+//        end = 0; start = 0;
+//
+//       // Workload F: Free every other block
+//        start = clock();
+//        int mallocCounterF = 0;
+//        char* ptrF = NULL;
+//
+//        while(mallocCounterF < 150){
+//            ptrF = my_malloc(1);
+//            mallocCounter++;
+//        }
+//        while(mallocCounterF > 0){
+//            if(mallocCounterF%2 ==0){
+//                my_free(ptrF);
+//            }
+//            mallocCounterF --;
+//        }
+//        end = clock();
+//
+//        //Manage RunTimeF for iteration
+//        elapsedF = (double)(end - start);
+//        totalRunTimeF = totalRunTimeF+ elapsedF;
+//        end = 0; start = 0;
+//        //increment program counter for next iteration
+//       programCounter++;
+//    }
+//
+    //Calculate average for each workload
+//    double avgTimeA = totalRunTimeA/100;     //avg run time for A
+//    printf("%s %f\n", "Average RunTime for A:", avgTimeA);
+//    double avgTimeB = totalRunTimeB/100;     //avg run time for B
+//    printf("%s %f\n", "Average RunTime for B:", avgTimeB);
+//    double avgTimeC = totalRunTimeC/100;     //avg run time for C
+//    printf("%s %f\n", "Average RunTime for C:", avgTimeC);
+//    double avgTimeD = totalRunTimeD/100;     //avg run time for D
+//    printf("%s %f\n", "Average RunTime for D:", avgTimeD);
+//    double avgTimeE = totalRunTimeE/100;     //avg run time for E
+//    printf("%s %f\n", "Average RunTime for E:", avgTimeE);
+//    double avgTimeF = totalRunTimeF/100;     //avg run time for F
+//    printf("%s %f\n", "Average RunTime for F:", avgTimeF);
+}
+    }
     return 0;
 }
-    //Workload A: Malloc() 1 byte and immediately free it x150
-    void workloadA(){
-     int i;
-     for (i = 0; i < 150;i++)
-     {
-     void * p = malloc(1);
-     free(p);
-     }
+
+
+// Determistic testing RANDOM TEST IS NOT GOOD
+int main2(){
+    printf("Size of metaData=%i\n",sizeof(metaData));
+
+    char* p1 = my_malloc(36);
+    char* p2 = my_malloc(16);
+
+    my_free(p1);
+    
+
+    p1=my_malloc(4);
+//my_free(p1);
+
+
+    
+    return 0;
 }
 
-    //Workload B: Malloc(), store pointer in arrayx150 times
-    void workloadB()
-    {
-     int i;
-     char * ptrs[150];
-     for(i = 0; i < 150;i++)
-     {
-     ptrs[i] = malloc(1);
-     }
-     freeall();
-    }
-
-    //Workload C: Randomly choose b/w 1 byte malloc and 1 byte free, continue until 150 mallocs
-    void workloadC(){
-     int mallocCounter = 0;  //counts total number of mallocs
-     char*p = NULL;
-     while(mallocCounter <= 150){
-        if(mallocCounter == 0){ //if no byte has been allocated, then first malloc
-            p = malloc(1);
-            mallocCounter++;
-            if (mallocCounter >= 150)break;
-        }
-        int r = rand()%2;
-        if(r==1){           //if r = 1 malloc 1 byte
-            p = malloc(1);
-            mallocCounter++;
-            if (mallocCounter >= 150)break;
-        }
-        else
-            {
-                metaData * a = blockPtr;
-                while (a != NULL)
-                    {
-                    if(!(a->isFree))
-                    {
-                        free(++a);
-                        break;
-                    }
-                    a = a->next;
-                    }
+int main(){
+    printf("Size of metaData=%i\n",sizeof(metaData));
+    
+    int MCOUNT=150;
+    int mallocCtr=0;
+    int freeCtr = 0;
+    
+    
+    while(mallocCtr < MCOUNT){
+        int operation = rand() % 2;
+        printf("operation: %d\n", operation);
+        if(operation == 1){
+           // my_malloc(1);
+            
+            int sizer = rand() % 64;
+            
+            printf("Sizer=%i",sizer);
+            my_malloc(sizer);
+            
+            mallocCtr++;
+        } else {
+            // Free
+            metaData*  root = getBlockPointer();
+            while(root->next != NULL && root->next->isFree){
+                root = root->next;
             }
-            printblocks();
-     }
-     printblocks();
-     freeall();
-    }
-    //Workload D: Randomly-sized malloc() or free() pointers
-    void workloadD(){
-    int mallocSize = 0; //size of malloc bytes
-    int operation = 0;  //determines free or malloc
-    int mallocCounter = 0;     //keeps track of total number of malloc
-    char * ptr = NULL;
-    while(mallocCounter < 150){
-        if(mallocCounter == 0){
-            mallocSize = rand()%64;
-            ptr= malloc(mallocSize);
-            mallocCounter++;
-            printblocks();
-        }
-        operation = rand()%2;
-        if(operation ==1){
-            mallocSize = rand()%64;
-            ptr = malloc(mallocSize);
-            mallocCounter++;
-            printblocks();
-        }
-        else{
-            metaData * a = blockPtr;
-                while (a != NULL)
-                    {
-                    if(!(a->isFree))
-                    {
-                        free(++a);
-                        break;
-                    }
-                    a = a->next;
-                    }
-            mallocCounter--;
-            printblocks();
+            
+            if(root != NULL){
+                my_free(root->next);
+                freeCtr++;
+            } else {
+                printf("Nothing to free\n");
+            }
         }
     }
-   freeall();
-    }
-//Workload E: Malloc randomly sized chunk random number of times (size of chunk stays consistent) and free
-    void workloadE(){
-    void * ptr = NULL;
-    int mallocSize = rand()%5000;    //size of malloc chunks
-    int mallocCounter = rand()%150;    //total number of malloc sizes (must be b/w 1 and 150)
-    int remainingSize = blockPtr->size;     //remaining size of memory after each chunk is malloc
-    int counter = 0;
-    while(counter < mallocCounter){
-        if(remainingSize - mallocSize < 0)break;
-        ptr = malloc(mallocSize);
-        counter++;
-    }
-    freeall();
-    }
-
-//Workload F: Malloc random number of times, free half that number of times - continue 150 times
-//    int mallocCounter = rand()%150;
-//    int counter = 0;
-//    char* ptr = NULL;
-//    while(counter != mallocCounter){
-//        ptr = my_malloc(1);
-//        counter++;
-//    }
-//    counter = 0;
-//    while(counter != (mallocCounter * .5)){
-//        my_free(ptr);
-//        counter++;
-//    }
-
-
+    printf("TEST Completed Malloc=%d Free=%d\n",mallocCtr, freeCtr);
+    return 0;
+}
--- mymalloc.c
+++ mymalloc.c
@@ -1,51 +1,116 @@
 #include<stdio.h>
 #include<stddef.h>
 #include "mymalloc.h"
-#define malloc( x ) my_malloc( x, __FILE__, __LINE__ )
-#define free( x ) my_free( x, __FILE__, __LINE__ )
-static char myblock[5000];  //array size simulated
-metaData *blockPtr = (void*) myblock;  //sets pointer to first index in arrays
+
 void initialize(){
-    blockPtr->size = 5000 - sizeof(metaData);
+    blockPtr->size = ARRAY_SIZE - sizeof(metaData);
     blockPtr->isFree = 1;
     blockPtr->next = NULL;
+    blockPtr->isFirstBlock=1;
+    printf("Memory Initialized\n");
 }
-//
-////if first block found is greater than required size, then split
-void allocate(metaData *largeBlock, int requiredSize){
-    metaData *newBlock = (void*)((void *)largeBlock + requiredSize + sizeof(metaData));//new block is pointing to the space after the metadata and required space of large block
-    newBlock->size = largeBlock->size - requiredSize - sizeof(metaData);//new block size is equal to how much space is left in large block after required space and metadata are inserted in
-    largeBlock->size = requiredSize; //large block now becomes just a small block with size equal to its required size
+
+//if first block found is greater than required size, then split
+void allocate(metaData *current, int size){
+//    printf("ALLOC Called %p   %i\n",current,size);
+    
+    
+    metaData *newBlock = (void*)((void *)current + size + sizeof(metaData));//new block is pointing to the space after the metadata and required space of large block
+    newBlock->size = current->size - size - sizeof(metaData);//new block size is equal to how much space is left in large block after required space and metadata are inserted in
+    current->size = size; //large block now becomes just a small block with size equal to its required size
     newBlock->isFree = 1; //slot is still free
-    largeBlock->isFree = 0; //large slot is no longer free becuase it has been previously malloced
-    newBlock->next = largeBlock->next; //newblock is now "after" large block in the mem LL so it should inherit large block's next node
-    largeBlock->next = newBlock; //large block is now "before" new block so its next node is new block
+    current->isFree = 0; //large slot is no longer free becuase it has been previously malloced
+    newBlock->next = current->next;
+    current->next = newBlock; //newblock is now "after" large block in the mem LL so it should inherit large block's next node
+    
+    newBlock->isFirstBlock=0;
+    
+    traverse(" Malloc ");
 }
-//
-void* my_malloc(int size, char * File, int Line)
-{
-    if(!blockPtr->size)initialize();//executes when the isFree value in the structure is NULL. This only happens once.
-    metaData *current = blockPtr;
-    while(current != NULL)
-    {
-    if((current->size == size) && current->isFree)
-    {
-        current->isFree = 0;
-        current++; //move the current pointer past all the metaData and have it point to beginning of the allocated space
-        return (void *)current;
-    }
-    else if (current->size >(size + sizeof(metaData)))
-    {
-        allocate(current, size);
-        current++; //move the current pointer past all the metaData and have it point to beginning of the allocated space
-        return (void *)current;
-    }
-    current = current->next;
-    }
-    fprintf(stderr,"Error in file: %s Line: %d. Not Enough Space\n", File, Line);
-    return NULL;
+
+void* my_malloc(int size){
+        if(!memoryInitializedFlag){
+            initialize();//executes when the isFree value in the structure is NULL. This only happens once.
+            memoryInitializedFlag=1;
+        }
+    
+    if(size == 0) return NULL;
+    
+        metaData *current = blockPtr;
+    
+        while(current != NULL)
+        {
+            if(current->isFree){
+                if(current->size == size){
+                    // we do not need to create the meta data block
+                    // simply mark the block as NOT free
+                    current->isFree = 0;
+                    return current;
+                } else if (current->size > (size + sizeof(metaData))){
+                    // New metadata block needs to be created ... so call allocate
+                    allocate(current,size);
+                    return current;
+                } else {
+                    // This block is smaller than what we need so lets check next
+                    current = current->next;
+                }
+            } else {
+                current = current->next;
+            }
+        }
+    return current;
 }
+
+//void* my_malloc(int size)
+//{
+//    if(!memoryInitializedFlag){
+//        initialize();//executes when the isFree value in the structure is NULL. This only happens once.
+//        memoryInitializedFlag=1;
+//    }
+//    metaData *current = blockPtr;
+//    while(current != NULL)
+//    {
 //
+//        if((current->size == size) && current->isFree)
+//        {
+//           // printf("WHYYYYY\n");
+//
+//            //current++; //move the current pointer past all the metaData and have it point to beginning of the allocated space
+////
+////            if(current->isFirstBlock && current->next==NULL && current->isFree){
+////                metaData *newBlock = (void*)((void *)current  + sizeof(metaData));
+////                newBlock->size=current->size - sizeof(metaData);
+////                newBlock->isFree=1;
+////                newBlock->isFirstBlock=0;
+////                newBlock->next=NULL;
+////                current->size=0;
+////                current->isFree=0;
+////                current->next=newBlock;
+////
+////                current=newBlock;
+////            }
+//
+////            current->isFree = 0;
+////            printf("Space has been allocated %p\n", current);
+//
+//            allocate(current, size);
+//
+//            //printf("Validity=%i",isPointerValid(current));
+//
+//            return (void *)current;
+//        }
+////        else if (current->size >(size + sizeof(metaData)))
+////        {
+////            allocate(current, size);
+////            printf("%s", "Space has been allocated\n");
+////            current++; //move the current pointer past all the metaData and have it point to beginning of the allocated space
+////            return (void *)current;
+////        }
+//        current = current->next;
+//    }
+//    puts("Not enough memory. Malloc error");
+//    return NULL;
+//}
 
 void merge(){
     metaData *current = blockPtr->next;
@@ -57,6 +122,7 @@ void merge(){
             prev->isFree = 1;
             current=current->next;
             prev->next = current;
+            printf("%s", "MERGED\n");
         }
         else
         {
@@ -66,33 +132,47 @@ void merge(){
     }
 
 }
-void my_free(void* p, char * File, int Line){
-    if(((void *)myblock > p) || ((void*)(myblock + 5000) < p))
+
+int isPointerValid(void* p){
+    if( (char*)p >= myblock && (char*)p < myblock+ARRAY_SIZE)
+        return 1;
+    else
+        return 0;
+}
+
+void my_free(void* p){
+    //if pointer is not located within array
+//    if(((void *)myblock > p) || ((void*)(myblock + ARRAY_SIZE) < p))
+    if(!isPointerValid(p))
     {
-        fprintf(stderr,"Error in file: %s on Line: %d. Cannot free pointer that was not allocated by malloc\n", File, Line);
-        exit(0);
+        printf("my_free   invalid pointer %p",p);
+        return;
     }
     else
     {
         metaData* current = p;
-        current--;
+//        current--;
         if(current-> isFree){
-           fprintf(stderr,"Error in file: %s on Line: %d. Pointer is already freed\n", File, Line);
-           exit(0);
+            printf("my_free-2  point points to free memory %p \n", current);
+            return;
         }
         current->isFree = 1;
         merge();
+        
+        traverse(" Free ");
+        
         return;
     }
 
 }
-void printblocks()
+
+void printblocks(char* location)
 {
     metaData * p = blockPtr;
     int counter = 1;
     while(p!=NULL)
     {
-        printf("Block %d: size: %d  Free:%d\n", counter, p->size, p->isFree);
+        printf("Block %d: location: %s size: %d  Free:%d\n", counter, location, p->size, p->isFree);
         counter++;
         p = p->next;
     }
@@ -100,17 +180,38 @@ void printblocks()
 
 void freeall()
 {
-    metaData * p = blockPtr;
-    while (p != NULL)
-    {
-        if(!(p->isFree))
-        {
-            free(++p);
-            p--;
-        }
-
-        p = p->next;
-    }
+    blockPtr->isFree= 1;
+    blockPtr->size = ARRAY_SIZE-sizeof(metaData);
+    blockPtr->next = NULL;
+//    metaData * p = blockPtr;
+//    while (p != NULL)
+//    {
+//        if(!(p->isFree))
+//        {
+//            my_free(++p);
+//            p--;
+//        }
+//
+//        p = p->next;
+//    }
 }
 
+metaData* getBlockPointer(){
+    return blockPtr;
+}
+
+void traverse(char* location){
+    
+    metaData* x = blockPtr;
+    printf("===============BLK PTR=%p============\n", blockPtr);
+    
+    printf("TRAVERSAL %s \n[blk=0, size=%d, free=%d next=%p  me=%p]\n",location, x->size,x->isFree,x->next,x);
+    int ctr = 0;
+    while(x->next != NULL){
+        x = x->next;
+        ctr++;
+        printf("[blk=%d, size=%d, free=%d next=%p  me=%p]\n",ctr,x->size,x->isFree,x->next,x);
+    }
+    printf("===================================\n");
+}
 
